30cf008916dbf5f193eb21d201f0ba88defb9d4a
Asaf Gabai <asafg@jfrog.com>
2022-03-24 09:34:23 +0200

    Added a function to find the host's OS.

--- client/src/api/utils.ts
+++ client/src/api/utils.ts
@@ -1,3 +1,5 @@
+let windowsSystem: boolean | undefined;
+
 export function throwErrorAsString(e: any) {
   let stringErr: string;
   if (e.stderr != undefined) {
@@ -7,4 +9,13 @@ export function throwErrorAsString(e: any) {
   }
   console.log(stringErr);
   throw stringErr;
+}
+
+export async function isWindows(): Promise<boolean> {
+  if (windowsSystem === undefined) {
+    const result = await window.ddClient.docker.cli.exec("info", ["--format", '"{{.OSType }}"']);
+    let osType = result.stdout.trim();
+    windowsSystem = osType === "windows";
+  }
+  return windowsSystem;
 }
