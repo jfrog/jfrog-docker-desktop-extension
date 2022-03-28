786ee1674b3d985feb73d67fbee40c09634cae7b
Asaf Gabai <asafg@jfrog.com>
2022-03-28 15:03:44 +0300

    Fixed permissions scripts.

--- binaries/unix/scanpermissions.sh
+++ binaries/unix/scanpermissions.sh
@@ -2,4 +2,4 @@
 
 export JFROG_CLI_HOME_DIR=~/.jfrog-docker-desktop-extension
 
-$(dirname "$0")/jf xr curl -X POST -H "Content-Type:application/json" -d "{\"component_details\":[{\"component_id\":\"testComponent\"}]}" api/v1/summary/component -s --output /dev/null -w %{http_code}
+$(dirname "$0")/jf xr curl --server-id validation -X POST -H "Content-Type:application/json" -d "{\"component_details\":[{\"component_id\":\"testComponent\"}]}" api/v1/summary/component -s --output /dev/null -w %{http_code}
