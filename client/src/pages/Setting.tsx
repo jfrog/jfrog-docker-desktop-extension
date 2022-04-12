c64a1556845dfddafdcf479d63f255e11a22b4e4
Or Geva <orgab@jfrog.com>
2022-04-12 11:00:24 +0300

    Bugfix - Settings page does not save scan policies

--- client/src/pages/Setting.tsx
+++ client/src/pages/Setting.tsx
@@ -10,7 +10,7 @@ import {
   Button,
   Typography,
 } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { ExtensionConfig } from '../types';
 import { useHistory } from 'react-router-dom';
 import { Load, Save } from '../utils/config';
@@ -20,6 +20,7 @@ import { LoadingButton } from '@mui/lab';
 import Loader from '../components/Loader';
 import { JfrogHeadline } from '../components/JfrogHeadline';
 import { SettingsForm } from '../components/Settings/Settings';
+import { isAwaitExpression } from 'typescript';
 
 export const SettingsPage = () => {
   let history = useHistory();
@@ -28,23 +29,22 @@ export const SettingsPage = () => {
   const [state, setValue] = useState<ExtensionConfig>({ authType: BASIC_AUTH });
   const [policy, setPolicy] = useState<Policy>(Policy.Vulnerabilities);
 
-  useEffect(() => {
-    if (isWindowLoading) {
-      Load()
-        .then((config) => {
-          setValue(config);
-          if (config.project) {
-            setPolicy(Policy.Project);
-          }
-          if (config.watches) {
-            setPolicy(Policy.Watches);
-          }
-        })
-        .finally(() => {
-          setWindowLoading(false);
-        });
+  const fetchData = useCallback(async () => {
+    const config = await Load();
+    setValue(config);
+    if (config.project != undefined) {
+      setPolicy(Policy.Project);
     }
-  });
+    if (config.watches != undefined) {
+      setPolicy(Policy.Watches);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData()
+      .then(() => setWindowLoading(false))
+      .catch(console.error);
+  }, [fetchData]);
 
   const HandleCancel = () => {
     history.push('/scan');
@@ -98,16 +98,17 @@ export const SettingsPage = () => {
                   {SettingsForm(state, setValue)}
                 </Stack>
                 <Box marginBottom={'30px'}>
-                  <Box sx={{
-                        fontFamily: 'Roboto',
-                        fontStyle: 'normal',
-                        fontWeight: 400,
-                        fontSize: '19px',
-                        marginTop: '25px',
-
-                      }}marginTop={6}>
-
-                      Scanning Policy
+                  <Box
+                    sx={{
+                      fontFamily: 'Roboto',
+                      fontStyle: 'normal',
+                      fontWeight: 400,
+                      fontSize: '19px',
+                      marginTop: '25px',
+                    }}
+                    marginTop={6}
+                  >
+                    Scanning Policy
                   </Box>
                   <RadioGroup
                     aria-labelledby="demo-radio-buttons-group-label"
@@ -116,17 +117,13 @@ export const SettingsPage = () => {
                   >
                     <FormControlLabel
                       value="allVulnerabilities"
-                      onChange={() => {
-                        setPolicy(Policy.Vulnerabilities);
-                      }}
+                      onChange={() => setPolicy(Policy.Vulnerabilities)}
                       control={<Radio />}
                       label="All Vulnerabilities"
                     />
                     <FormControlLabel
                       value="project"
-                      onChange={() => {
-                        setPolicy(Policy.Project);
-                      }}
+                      onChange={() => setPolicy(Policy.Project)}
                       control={<Radio />}
                       label="JFrog Project"
                     />
@@ -142,9 +139,7 @@ export const SettingsPage = () => {
                     </Box>
                     <FormControlLabel
                       value="watches"
-                      onChange={() => {
-                        setPolicy(Policy.Watches);
-                      }}
+                      onChange={() => setPolicy(Policy.Watches)}
                       control={<Radio />}
                       label="Watches"
                     />
