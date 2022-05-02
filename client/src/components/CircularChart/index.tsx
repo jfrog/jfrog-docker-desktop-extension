85e997b7d6983637e8fc2222022531c37bb7b8f2
Michael Sverdlov <sverdlov93@gmail.com>
2022-05-02 15:47:28 +0300

    Scan page (#34)
    * add summary tooltip and xray id
    
    * fix search field
    
    * fix missing images with different tag
    
    * Add vulns pie chart
    
    * improve piechart
    
    * improve pie chart
    
    * add ImpactPath chart
    
    * Add docker-mui-them
    
    * update cli to 2.16.2
    
    * add fail=false for context scans
    
    * and piechart and impact path data
    
    * improve scan page

---
+++ client/src/components/CircularChart/index.tsx
@@ -0,0 +1,89 @@
+import { Box, styled, Typography, useTheme } from '@mui/material';
+import { useState } from 'react';
+import packageIcon from '../../assets/package.png';
+const chartColors = ['#fb515b', '#c9d0e3', '#d8ddea', '#eef0f6'];
+const CORE_CIRCLE_SIZE = 35;
+
+export default function CircularChart({ items }: { items: string[] }) {
+  const [hovered, setHovered] = useState<number | undefined>(undefined);
+  const { palette } = useTheme();
+  const isDarkMode = palette.mode == 'dark';
+  const centerTop = (CORE_CIRCLE_SIZE / 2) * items.length;
+  return (
+    <Box
+      padding="0 10px"
+      overflow="hidden"
+      display="flex"
+      flexDirection="column"
+      width={1 / 3}
+      height={`${(CORE_CIRCLE_SIZE + 30) * items.length}px`}
+      position="relative"
+    >
+      {items?.map((item, index) => {
+        const isCoreItem = index == 0;
+        return (
+          <StyledCircle
+            key={index}
+            lineHeight={`${15 * (items.length - 1) + index * 25}px`}
+            topLine={`${CORE_CIRCLE_SIZE - index + index * 20}px`}
+            lineBorderColor={isCoreItem ? '#fb515b' : '#c7cee0'}
+            top={`${centerTop}px`}
+            sx={{ backgroundColor: chartColors[index >= chartColors.length ? chartColors.length - 1 : index] }}
+            width={`${CORE_CIRCLE_SIZE * (index + 1)}px`}
+            height={`${CORE_CIRCLE_SIZE * (index + 1)}px`}
+            zIndex={1999 - index}
+          >
+            <StyledCircleTextWrapper top={`${45 + 15 * (items.length - 1) + index * 45}px`}>
+              <img src={packageIcon} alt="" height="24px" width="auto" style={{ marginRight: '5px' }} />
+              <StyledCircleText fontSize="14px" color={isCoreItem ? '#fb515b' : 'inherit'}>
+                {item}
+              </StyledCircleText>
+            </StyledCircleTextWrapper>
+          </StyledCircle>
+        );
+      })}
+    </Box>
+  );
+}
+
+const StyledCircle = styled(Box)<{ topLine: string; lineHeight: string; lineBorderColor: string }>`
+  position: absolute;
+  left: 50%;
+  display: inline-block;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+  transition: all 0.5s ease-in-out;
+  box-shadow: inset 0 0 24px rgb(0 0 0 / 8%);
+
+  &:after {
+    content: '';
+    position: absolute;
+    display: block;
+    border-top: 2px solid ${({ lineBorderColor }) => lineBorderColor};
+    border-left: 2px solid ${({ lineBorderColor }) => lineBorderColor};
+    height: ${({ lineHeight }) => lineHeight};
+    width: 36px;
+    top: ${({ topLine }) => topLine};
+    right: 99%;
+    border-radius: 5px 0 0 0;
+    transform: skewY(-45deg);
+  }
+`;
+
+const StyledCircleTextWrapper = styled(Box)`
+  left: -43px;
+  align-items: center;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  z-index: 10000;
+  white-space: nowrap;
+  transition: all 0.2s;
+`;
+
+const StyledCircleText = styled(Typography)`
+  max-width: 250px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
--- client/src/components/Loader/index.tsx
+++ client/src/components/Loader/index.tsx
@@ -1,11 +1,33 @@
 import jfrogLogo from '../../assets/jfrog_logo.png';
+import { styled } from '@mui/material/styles';
+import { keyframes } from '@mui/system';
+import { Box } from '@mui/material';
 
 const Loader = () => {
   return (
-    <div className="jf-loader">
-      <img src={jfrogLogo} alt="" className="jf-loader-img" />
-    </div>
+    <Box display="flex" alignItems="center" justifyContent="center">
+      <BouncingJfrog src={jfrogLogo} alt="" />
+    </Box>
   );
 };
+
+const heartbeat = keyframes`
+from {
+  transform: scale3d(1, 1, 1);
+}
+
+50% {
+  transform: scale3d(1.15, 1.15, 1.15);
+}
+
+to {
+  transform: scale3d(1, 1, 1);
+}
+`;
+
+const BouncingJfrog = styled('img')`
+  width: 60px;
+  animation: ${heartbeat} 1.7s infinite;
+`;
 
 export default Loader;
