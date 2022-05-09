5142c6a5f3447c69677726364ba76637c1c245e0
Asaf Gabai <asafg@jfrog.com>
2022-05-09 13:17:01 +0300

    setupEnv gets a function that is called when the user's environment is being built. Added labels to Dockerfile.

--- client/src/api/setup-env.ts
+++ client/src/api/setup-env.ts
@@ -1,39 +1,34 @@
 import {execOnHostAndStreamResult} from "./utils";
 
-export const enum SetupStage {
-  Idle,
-  WaitingForUser,
-  PreparingEnv,
-  Done,
-  Error
-}
-
-export var setupStage: SetupStage = SetupStage.Idle;
-
 /**
  * Setups a new JFrog environment. It opens a registration form in a browser window and saves the environments details in the configuration.
  * After calling this function, use the setupStage public property to check out the stage of the setup process.
  */
-export async function setupEnv(): Promise<void> {
-  setupStage = SetupStage.WaitingForUser;
-  console.log("Running setup command");
-  await execOnHostAndStreamResult('runcli.sh', 'runcli.bat', ["setup", "--format=machine"], {
-    stream: {
-      splitOutputLines: true,
-      onOutput(data: { stdout: string; stderr?: undefined } | { stdout?: undefined; stderr: string }): void {
-        if (data.stdout === "PREPARING_ENV") {
-          setupStage = SetupStage.PreparingEnv;
-          console.log("The new environment is being built");
-        }
+export async function setupEnv(setPreparingEnv: () => void): Promise<void> {
+  return new Promise((resolve, reject) => {
+    console.log('Running setup command');
+    execOnHostAndStreamResult('runcli.sh', 'runcli.bat', ['setup', '--format=machine'], {
+      stream: {
+        splitOutputLines: true,
+        onOutput(data: { stdout: string; stderr?: undefined } | { stdout?: undefined; stderr: string }): void {
+          if (data.stdout === 'PREPARING_ENV') {
+            console.log('The new environment is being built');
+            setPreparingEnv();
+          }
+        },
+        onError(error: any): void {
+          console.error(error);
+          reject(error);
+        },
+        onClose(exitCode: number): void {
+          console.log('Setup command finished with exit code ' + exitCode);
+          if (exitCode === 0) {
+            resolve();
+          } else {
+            reject("Setup failed");
+          }
+        },
       },
-      onError(error: any): void {
-        console.error(error);
-        setupStage = SetupStage.Error;
-      },
-      onClose(exitCode: number): void {
-        console.log("Setup command finished with exit code " + exitCode);
-        setupStage = SetupStage.Done;
-      }
-    }
+    });
   });
 }
