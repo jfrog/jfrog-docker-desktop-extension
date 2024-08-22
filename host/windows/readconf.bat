e7b27bcb5f24db75c810186747333ffb0f342a0c
Eyal Kapon <eyalk@jfrog.com>
2024-08-22 09:26:03 +0300

    added a modification that will maybe work

--- host/windows/readconf.bat
+++ host/windows/readconf.bat
@@ -5,4 +5,9 @@
 set "HOME_DIR=%USERPROFILE%\.jfrog-docker-desktop-extension"
 set "CONF_FILE=%HOME_DIR%\jfrog-docker-desktop-extension.conf"
 
-type %CONF_FILE%
+:: Debug: Echo the paths to ensure they are correct.
+echo HOME_DIR is set to: "%HOME_DIR%"
+echo CONF_FILE is set to: "%CONF_FILE%"
+
+:: Use quotes around the CONF_FILE variable to handle paths with spaces.
+type "%CONF_FILE%"
